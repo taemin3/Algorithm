6b02220b6a42ffdf33d02bde29a49003afd114f4
KIM TAEMIN <xoals8534@gmail.com>
2025-08-25 10:47:45 +0900

    [level 2] Title: 시소 짝꿍, Time: 83.04 ms, Memory: 119 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int[] weights) {
+        long answer = 0;
+        
+        Arrays.sort(weights);
+        
+        Map<Double,Integer> map = new HashMap<>();
+        
+        for (int w : weights) {
+            double a = (w * 2.0) / 3.0;
+            double b = (w * 1.0) / 2.0;
+            double c = (w * 3.0) / 4.0;
+            double d = (w * 1.0);
+            
+            if (map.containsKey(a)) {
+                answer += map.get(a);    
+            }  if (map.containsKey(b)) {
+                answer += map.get(b);   
+            }  if (map.containsKey(c)) {
+                answer += map.get(c);   
+            }  if (map.containsKey(d)) {
+                answer += map.get(d);   
+            }
+            
+            map.put(d,map.getOrDefault(d,0) + 1);
+        }
+        
+        return answer;
+    }
+}

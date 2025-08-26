aaf96c15d77911ace9bd271e2656d756c220e486
KIM TAEMIN <xoals8534@gmail.com>
2025-08-26 12:24:18 +0900

    [level 2] Title: 디펜스 게임, Time: 100.59 ms, Memory: 146 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = enemy.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int i = 0; i < enemy.length;i++) {
+            pq.offer(enemy[i]);
+            n -= enemy[i];
+            if (n < 0) {
+                if (k > 0) {
+                    n += pq.poll();
+                    k--;
+                    continue;
+                }
+                answer = i;
+                break;
+            }
+            
+            
+        }
+        return answer;
+    }
+}

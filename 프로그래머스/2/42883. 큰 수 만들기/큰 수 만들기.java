220cd417dcfd91731f2d59b308d760bf33b2733f
KIM TAEMIN <xoals8534@gmail.com>
2025-08-29 19:25:47 +0900

    [level 2] Title: 큰 수 만들기, Time: 7058.38 ms, Memory: 91.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String number, int k) {
+        
+        StringBuilder answer = new StringBuilder();
+        
+        int idx=0;
+        for(int i=0;i<number.length()-k;i++) {
+            int max = 0;
+            for(int j=idx; j<=i+k; j++) {
+                if(max < number.charAt(j)-'0') {
+                    max = number.charAt(j)-'0';
+                    idx = j+1;
+                }
+            }
+              
+            answer.append(max);
+        }
+        
+        return answer.toString();
+        }
+}

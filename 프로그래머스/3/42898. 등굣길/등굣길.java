e1b3bf1f247c9a7e17fdb40285dd0417719b027d
KIM TAEMIN <xoals8534@gmail.com>
2025-11-20 21:01:14 +0900

    [level 3] Title: 등굣길, Time: 0.69 ms, Memory: 54.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    
+    public int solution(int m, int n, int[][] puddles) {
+        int [][] dp = new int[m + 1][n + 1];
+        boolean [][] visit = new boolean[m+1][n+1];
+        
+        
+        for (int i = 0; i < puddles.length ; i++) {
+            visit[puddles[i][0]][puddles[i][1]] = true;
+        }
+        
+        dp[1][1] = 1;
+        
+        for (int i = 1; i <= m ;i++) {
+            for (int j = 1 ; j <= n ; j++) {
+                if (i == 1 && j == 1) {
+                    continue;
+                }
+                if (!visit[i][j])  {
+                    dp[i][j] = (dp[i - 1][j] + dp[i][j-1]) % 1000000007;
+                }
+             }
+        }
+    
+        return dp[m][n];
+    }
+    
+    
+}

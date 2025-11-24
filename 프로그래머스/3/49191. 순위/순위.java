b797d18646910a61c5ea8a53469663f7edf15589
KIM TAEMIN <xoals8534@gmail.com>
2025-11-24 17:00:15 +0900

    [level 3] Title: 순위, Time: 8.62 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int solution(int n, int[][] results) {
+        boolean[][] win = new boolean[n + 1][n + 1];
+        
+        for (int [] r : results) { 
+            win[r[0]][r[1]] = true;
+        }
+        
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                if (!win[i][k]) {
+                    continue;
+                }
+                for (int j = 1; j <= n;j++) {
+                    if (win[k][j]) {
+                        win[i][j] = true;
+                    }
+                }
+            }
+            
+        }
+        
+        
+        
+        int answer = 0;
+        
+        for (int i =1;i <= n ;i++) {
+            int count = 0;
+            for (int j = 1;j <= n;j++) {
+                if (i == j) continue;
+                if (win[i][j] || win[j][i]) {
+                    count++;
+                }
+            }
+            
+            if (count == n - 1) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}

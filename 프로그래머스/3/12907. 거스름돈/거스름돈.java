dadc23613f53b752c2dab2aae9aa49072d7553ba
KIM TAEMIN <xoals8534@gmail.com>
2025-11-26 17:14:35 +0900

    [level 3] Title: 거스름돈, Time: 60.31 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n, int[] money) {
+        int answer = 0;
+        int [] dp = new int[n+1];
+        dp[0] = 1;
+        for (int m : money) {
+            for (int i = m; i <= n;i++) {
+                dp[i] += dp[i - m];
+                dp[i] %= 1000000007;
+            }
+        }
+        return dp[n];
+        
+        
+    
+    }
+}

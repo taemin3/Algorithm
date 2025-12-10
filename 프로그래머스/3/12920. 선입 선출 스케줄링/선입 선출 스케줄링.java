1768331b8f312cfba095d1fab4af9633eb7385f3
KIM TAEMIN <xoals8534@gmail.com>
2025-12-10 17:06:31 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 7.97 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.java
+++ 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.java
@@ -1,45 +1,36 @@
 class Solution {
     public int solution(int n, int[] cores) {
-        int answer = 0;
         
-        int m = cores.length;
-        
-        long left = 0;
-        long right = 10000*n;
-        
-        while(left < right) {
-            long sum = m;
-            long mid = (left+right)/ 2;
+        long low = 1;
+        long high = cores.length * 10000;
+        long anw = 0;
+        while(low <= high) {
+            long sum = cores.length;
+            long mid = (low + high) / 2;
             for (int c : cores) {
                 sum += mid/c;
             }
             if (sum >= n) {
-                right = mid;
+                anw = mid;
+                high = mid - 1;
             } else {
-                left = mid + 1;
+                low = mid + 1;
             }
         }
         
-        long T = left; 
-
+        long before = cores.length;
+        for (int c : cores) {
+            before += (anw - 1) / c;
+        }
+        long remain = n - before;
         
-        long startedBeforeT = m;
-        for (int c : cores) startedBeforeT += (T - 1) / c;
-
-        long remain = n - startedBeforeT;
-
-       
-        for (int i = 0; i < m; i++) {
-            if (T % cores[i] == 0) {
+        for (int i = 0; i < cores.length;i++) {
+            if (anw % cores[i] == 0) {
                 remain--;
                 if (remain == 0) return i + 1;
             }
         }
         
         return -1;
-        
-        
-        
-        
     }
 }

e0ebbedc2549475f064132eda646a2bc6fd8cc5c
KIM TAEMIN <xoals8534@gmail.com>
2025-12-11 16:37:00 +0900

    [level 3] Title: 카운트 다운, Time: 39.47 ms, Memory: 93 MB -BaekjoonHub

---
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int[] solution(int target) {
+        int[] answer = {};
+        int INF = 100000000;
+        int [][] dp = new int[target + 1][2];
+        for (int i = 0 ; i <= target ; i++) {
+            dp[i][0] = INF;
+            dp[i][1] = 0;
+        }
+        
+        dp[0][0] = 0;
+        
+        for (int i = 1; i <= target;i++) {
+            if (i - 50 >= 0 && dp[i - 50][0] != INF) {
+                int dart = dp[i - 50][0] + 1;
+                int sin = dp[i-50][1] + 1;
+                if (dart < dp[i][0]) {
+                    dp[i][0] = dart;
+                    dp[i][1] = sin;
+                } else if(dart == dp[i][0]) {
+                    dp[i][1] = Math.max(dp[i][1],sin);
+                }
+            }
+            
+            for (int j = 1; j <= 20;j++) {
+                if (i - j >= 0 && dp[i - j][0] != INF) {
+                    int dart = dp[i-j][0] + 1;
+                    int sin = dp[i-j][1] + 1;
+                    if (dart < dp[i][0]) {
+                        dp[i][0] = dart;
+                        dp[i][1] = sin;
+                    } else if(dart == dp[i][0]) {
+                        dp[i][1] = Math.max(dp[i][1],sin);
+                    }
+                }
+                
+                if (i - j * 2>=0 && dp[i - j * 2][0] != INF) {
+                    int dart = dp[i -j * 2][0] +1;
+                    int sin = dp[i - j * 2][1];
+                    if (dart < dp[i][0]) {
+                        dp[i][0] = dart;
+                        dp[i][1] = sin;
+                    } else if(dart == dp[i][0]) {
+                        dp[i][1] = Math.max(dp[i][1],sin);
+                    }
+                }
+                
+                if (i - j * 3>=0 && dp[i - j * 3][0] != INF) {
+                    int dart = dp[i -j * 3][0] +1;
+                    int sin = dp[i - j * 3][1];
+                    if (dart < dp[i][0]) {
+                        dp[i][0] = dart;
+                        dp[i][1] = sin;
+                    } else if(dart == dp[i][0]) {
+                        dp[i][1] = Math.max(dp[i][1],sin);
+                    }
+                }
+            }
+        }
+        
+        return new int[]{dp[target][0],dp[target][1]};
+    }
+}

c5f257e29fb6bb28d570ca795dcaecfc46e1a09d
KIM TAEMIN <xoals8534@gmail.com>
2025-12-19 14:53:33 +0900

    [Gold IV] Title: 알파벳, Time: 1380 ms, Memory: 295124 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -5,7 +5,7 @@ import java.io.*;
 
 class Main {
     static char [][] map;
-    static boolean [][] visit;
+    static boolean [] visit;
     static int r, c;
     static int max = 0;
     static Set<Character> set;
@@ -14,11 +14,11 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
-
+        
         set = new HashSet<>();
 
         map = new char[r][c];
-        visit = new boolean[r][c];
+        visit = new boolean[26];
 
         for (int i =0; i < r;i++) {
             String str = br.readLine();
@@ -26,7 +26,7 @@ class Main {
                 map[i][j] = str.charAt(j);
             }
         }
-        set.add(map[0][0]);
+        visit[map[0][0] - 'A'] = true;
         bfs(0,0,1);
         System.out.println(max);
     }
@@ -45,10 +45,10 @@ class Main {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 if (nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
-                if (set.contains(map[nx][ny])) continue;
-                set.add(map[nx][ny]);
+                if (visit[map[nx][ny] - 'A']) continue;
+                visit[map[nx][ny] - 'A'] = true;
                 bfs(nx,ny,w + 1);
-                set.remove(map[nx][ny]);
+                visit[map[nx][ny] - 'A'] = false;
             
             }
         

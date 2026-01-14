bbfc950a9da0cba9a4146ef155b46428c999d2be
KIM TAEMIN <xoals8534@gmail.com>
2026-01-14 18:04:57 +0900

    [Gold III] Title: LCS 2, Time: 132 ms, Memory: 18796 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        int n = str1.length();
+        int m = str2.length();
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1 ; i <= n;i++) {
+            for (int j = 1 ; j <= m;j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i- 1][j -1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int i = n;
+        int j = m;
+        while(i > 0 && j > 0) {
+            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                sb.append(str1.charAt(i-1));
+                i--;
+                j--;
+            } else {
+                if (dp[i-1][j] >= dp[i][j-1]) i--;
+                else j--;
+            }
+            
+        }
+
+        System.out.println(dp[n][m]);
+        System.out.println(sb.reverse().toString());
+        
+    }
+}

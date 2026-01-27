22af66d6d9f415550f6e700d03a33121f8f8337b
KIM TAEMIN <xoals8534@gmail.com>
2026-01-27 16:05:31 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 520 ms, Memory: 69356 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -2,62 +2,68 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+// The main method must be in a class named "Main".
 class Main {
-   
+
     static List<int[]>[] graph;
-    static int n ;
-    static int INF = 200000000;
-    public static void main(String[] args) throws Exception{
+    static int N;
+    static int INF = 20000000;
+    
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
-        graph = new ArrayList[n + 1];
-        for (int i = 1; i <= n;i++) {
+
+        N = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N;i++) {
             graph[i] = new ArrayList<>();
         }
 
-        for (int i = 0 ; i < e;i++) {
-            st = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < E;i++) {
+             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            graph[a].add(new int[]{b, w});
-            graph[b].add(new int[] {a, w});
-            
+            int c = Integer.parseInt(st.nextToken());
+
+            graph[a].add(new int[]{b,c});
+            graph[b].add(new int[]{a,c});
         }
-        st = new StringTokenizer(br.readLine());
+         st = new StringTokenizer(br.readLine());
         int v1 = Integer.parseInt(st.nextToken());
         int v2 = Integer.parseInt(st.nextToken());
-        long answer1 = dijk(1,v1) + dijk(v1,v2) + dijk(v2,n);
-        long answer2 = dijk(1,v2) + dijk(v2,v1) + dijk(v1,n);
-        long anw = Math.min(answer1,answer2);
-        anw = (anw >= INF) ? -1 : anw;
-        System.out.println(anw);
+        long ans1 = dijk(1,v1) + dijk(v1,v2) + dijk(v2,N);
+        long ans2 = dijk(1,v2) + dijk(v2,v1) + dijk(v1,N);
+
+        long ans = Math.min(ans1,ans2);
+        System.out.println(ans >= INF ? -1 : ans);
+        
+        
     }
 
-    public static int dijk(int start, int target) {
+    static int dijk(int s, int t) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        pq.offer(new int[]{start,0});
-        int [] dist = new int[n + 1];
-        
+        int [] dist = new int[N+1];
         Arrays.fill(dist,INF);
-        dist[start] = 0;
+        dist[s] = 0;
+        pq.offer(new int[]{s,0});
+
         while(!pq.isEmpty()) {
             int [] cur = pq.poll();
-            int c = cur[0];
-            int w = cur[1];
-            if (w > dist[c]) continue;
-            for (int[] g : graph[c]) {
-                int nw = g[1] + w;
-                if (nw < dist[g[0]]) {
-                    pq.offer(new int[]{g[0], nw});
-                    dist[g[0]] = nw;
+            int now = cur[0];
+            int cost = cur[1];
+            if (dist[now] != cost) continue;
+
+            if (now == t) {
+                return cost;
+            }
+            for (int [] nxt : graph[now]) {
+                if (dist[nxt[0]] > nxt[1] + cost) {
+                    dist[nxt[0]] = nxt[1] + cost;
+                    pq.offer(new int[]{nxt[0], nxt[1] + cost});
                 }
             }
         }
-        return dist[target];
+        return dist[t];
     }
-
-   
 }

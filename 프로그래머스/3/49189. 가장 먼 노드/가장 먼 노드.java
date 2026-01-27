8e91c16d104d49805c1c7cbae904d5e89d543c16
KIM TAEMIN <xoals8534@gmail.com>
2026-01-27 13:01:14 +0900

    [level 3] Title: 가장 먼 노드, Time: 33.89 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,48 +1,53 @@
 import java.util.*;
 class Solution {
-    List<ArrayList<Integer>> list = new ArrayList<>();
-    boolean [] visit;
-    int answer = 0;
-    int max = 0;
+    
+    static List<Integer>[] graph;
+    static boolean [] visit;
+    
+   
     public int solution(int n, int[][] edge) {
+        graph = new ArrayList[n+1];
+        for (int i = 1 ; i <= n;i++) {
+            graph[i] = new ArrayList<>();
+        }
+        visit = new boolean[n + 1];
         
-        visit = new boolean[n+1];
-        
-        for (int i = 0;i <= n;i++) {
-            list.add(new ArrayList<>());
+        for (int i = 0 ; i < edge.length;i++) {
+            int a = edge[i][0];
+            int b = edge[i][1];
+            graph[a].add(b);
+            graph[b].add(a);
+            
         }
         
-        
-        for (int[] e : edge) {
-            list.get(e[0]).add(e[1]);
-            list.get(e[1]).add(e[0]);
-        }
-        bfs(1,n);
-        
-        return answer;
+        return bfs();
     }
     
-    public void bfs(int start,int n) {
-        visit[start] = true;
+    public int bfs() {
+        
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{start, 0});
+        visit[1] = true;
+        q.offer(new int[]{1,0});
+        int max = 0;
+        int ans = 0;
         while(!q.isEmpty()) {
             int [] cur = q.poll();
-            if (max < cur[1]) {
-                max = cur[1];
-                answer = 1;
-            } else if (max == cur[1]) {
-                answer++;
+            int now = cur[0];
+            int cnt = cur[1];
+            if (cnt == max) {
+                ans++;
+            } else if (cnt > max) {
+                max = cnt;
+                ans = 1;
+            }
+            for (int g : graph[now]) {
+                if (visit[g]) continue;
+                visit[g] = true;
+                q.offer(new int[] {g, cnt + 1});
             }
             
-            for (int nxt : list.get(cur[0])) {
-                if (!visit[nxt]) {
-                    visit[nxt] = true;
-                    q.offer(new int[]{nxt, cur[1] + 1});
-                }
-            }
-            
-          
         }
+        return ans;
     }
+      
 }

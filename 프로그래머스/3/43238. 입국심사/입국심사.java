115d435c9d6a8e594872aafbdb8760e8ebedda6f
KIM TAEMIN <xoals8534@gmail.com>
2026-01-27 12:37:15 +0900

    [level 3] Title: 입국심사, Time: 105.47 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,30 +1,30 @@
 import java.util.*;
-
 class Solution {
     public long solution(int n, int[] times) {
         Arrays.sort(times);
-
+        int k = times.length;
         long lo = 1;
-        long hi = (long) times[times.length - 1] * n;
-        long ans = hi;
-
-        while (lo <= hi) {
+        long hi = (long) times[k - 1] * n;
+        long ans = 0;
+        while(lo <= hi) {
             long mid = (lo + hi) / 2;
-
+            
             long count = 0;
             for (int t : times) {
                 count += mid / t;
-                if (count >= n) break; // 오버플로우/불필요 계산 방지
+            
             }
-
+            
             if (count >= n) {
                 ans = mid;
-                hi = mid - 1;  // 더 작은 시간으로 가능한지
+                hi = mid - 1;
             } else {
-                lo = mid + 1;  // 시간이 부족
+                lo = mid + 1;
             }
         }
-
+        
         return ans;
+        
+        
     }
 }

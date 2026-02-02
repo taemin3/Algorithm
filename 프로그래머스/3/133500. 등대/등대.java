223628f40b72720ff1dbd6d7d08fbb252a0e4d0d
KIM TAEMIN <xoals8534@gmail.com>
2026-02-02 14:39:17 +0900

    [level 3] Title: 등대, Time: 76.11 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/등대.java
+++ 프로그래머스/3/133500. 등대/등대.java
@@ -1,40 +1,47 @@
 import java.util.*;
 class Solution {
     
-    int[][] dp;
-    boolean [] visit;
-    List<Integer>[] graph;
-    
+    static List<Integer>[] graph;
+    static boolean [] visit;
+    static int [][] dp;
+     
     public int solution(int n, int[][] lighthouse) {
-        int answer = 0;
-        visit = new boolean[n + 1];
-        dp = new int[n + 1][2];
-        graph = new ArrayList[n + 1];
-        for (int i = 1; i <= n;i++){
+        
+        graph = new ArrayList[n+1];
+        for (int i = 1;i <= n;i++) {
             graph[i] = new ArrayList<>();
         }
-        for (int[] l : lighthouse) {
-            graph[l[0]].add(l[1]);
-            graph[l[1]].add(l[0]);
+        visit = new boolean[n + 1];
+        
+        for (int i = 0; i < n - 1;i++) {
+            int a = lighthouse[i][0];
+            int b = lighthouse[i][1];
+            graph[a].add(b);
+            graph[b].add(a);
         }
+        dp = new int[n+1][2];
         dfs(1);
-        
-        
         return Math.min(dp[1][0],dp[1][1]);
+        
     }
     
     public void dfs(int u) {
         visit[u] = true;
         dp[u][0] = 0;
         dp[u][1] = 1;
-        
         for (int g : graph[u]) {
             if (!visit[g]) {
                 dfs(g);
-                
                 dp[u][0] += dp[g][1];
                 dp[u][1] += Math.min(dp[g][0],dp[g][1]);
             }
-        }
+            
+        } 
+        
     }
+    
 }
+    
+    
+    
+    

98f9c4841637f2b8a908c30e280a6d2a75858540
KIM TAEMIN <xoals8534@gmail.com>
2026-02-03 18:22:10 +0900

    [level 3] Title: 등산코스 정하기, Time: 151.57 ms, Memory: 167 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -1,12 +1,20 @@
 import java.util.*;
 class Solution {
+    
+    static List<int[]>[] graph;
+    static boolean[] isGate;
+    static boolean[] isSummit;
+    static int INF = 1_000_000_000;
+    
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-        int[] answer = {};
+        graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n;i++) {
+            graph[i] = new ArrayList<>();
+        }
+        isGate = new boolean[n+1];
+        isSummit = new boolean[n+1];
         
-        boolean [] isGate = new boolean[n + 1];
-        boolean [] isSummit = new boolean[n+ 1];
-        
-        for(int g :gates) {
+        for (int g : gates) {
             isGate[g] = true;
         }
         
@@ -14,63 +22,59 @@ class Solution {
             isSummit[s] = true;
         }
         
-        List<int[]>[] graph = new ArrayList[n + 1];
-        
-        for (int i = 1; i <= n;i++) {
-            graph[i] = new ArrayList<>();
-        }
-        
         for (int [] p : paths) {
             int a = p[0];
             int b = p[1];
-            int w = p[2];
+            int c = p[2];
             
             if (isGate[a] || isSummit[b]) {
-                graph[a].add(new int[]{b,w});
+                graph[a].add(new int[]{b,c});
             } else if (isGate[b] || isSummit[a]) {
-                graph[b].add(new int[]{a,w});
+                graph[b].add(new int[]{a,c});
             } else {
-                graph[a].add(new int[]{b,w});
-                graph[b].add(new int[]{a,w});
+                graph[a].add(new int[]{b,c});
+                graph[b].add(new int[]{a,c});
             }
-        }    
+        }
+        return dijk(n);
         
+    }
+    
+    public int[] dijk(int n) {
+        int ans1 = 0;
+        int ans2 = 1_000_000_000;
         int[] dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+        Arrays.fill(dist, INF);
         
-        for (int g : gates) {
-            pq.offer(new int[]{g,0});
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+        for (int i = 1; i <= n;i++) {
+            if (isGate[i]) {
+                dist[i] = 0;
+                pq.offer(new int[]{i , 0});
+            }
         }
         
         while(!pq.isEmpty()) {
-             int [] cur = pq.poll();
+            int [] cur = pq.poll();
+            int now = cur[0];
+            int cost = cur[1];
+            if (dist[now] != cost) continue;
+            if (isSummit[now]) continue;
             
-            if (cur[1] > dist[cur[0]]) continue;
-            
-            if (isSummit[cur[0]]) {
-                continue;
-            }
-            
-            for (int[] g : graph[cur[0]]) {
-                int max = Math.max(cur[1],g[1]);
-                if (max < dist[g[0]]) {
+            for (int [] g : graph[now]) {
+                int max = Math.max(cost, g[1]);
+                if (max < dist[g[0]]){
                     dist[g[0]] = max;
-                    pq.offer(new int[]{g[0], max});
+                    pq.offer(new int[]{g[0],max});
                 }
             }
         }
-        int anw1 = -1;
-        int anw2 = Integer.MAX_VALUE;
-        
-        Arrays.sort(summits);
-        for (int s : summits) {
-            if (dist[s] < anw2) {
-                anw1 = s;
-                anw2 = dist[s];
+        for (int i =1 ;i <= n;i++) {
+            if(isSummit[i] && dist[i] < ans2) {
+                ans1 = i;
+                ans2 = dist[i];
             }
         }
-    
-        return new int[]{anw1,anw2};
+        return new int[]{ans1,ans2};
     }
 }

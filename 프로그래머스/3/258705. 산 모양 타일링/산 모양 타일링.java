de42b51801c73c10ccc7367ccec941587731143a
KIM TAEMIN <xoals8534@gmail.com>
2026-02-06 17:57:25 +0900

    [level 3] Title: 산 모양 타일링, Time: 9.04 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -1,22 +1,19 @@
 class Solution {
     public int solution(int n, int[] tops) {
-        int answer = 0;
-        int [] dp0 = new int[n + 1];
         int [] dp1 = new int[n + 1];
+        int [] dp2 = new int[n + 1];
         
-        dp0[0] = 1;
-        dp1[0] = 0;
+        dp1[0] = 1;
+        dp2[0] = 0;
         
-        for (int i = 0 ; i < n; i++) {
-            dp1[i + 1] = (dp0[i] + dp1[i]) % 10007;
-            
+        for (int i = 0;i < n;i++) {
+            dp2[i + 1] = (dp1[i] + dp2[i]) % 10007;
             if (tops[i] == 1) {
-                dp0[i+1] = ((dp0[i] * 3) + (dp1[i] * 2)) % 10007; 
+                dp1[i+1] = ((dp1[i] * 3) + (dp2[i] *2)) % 10007;
             } else {
-                dp0[i+1] = ((dp0[i] * 2) + dp1[i]) % 10007;
+                dp1[i+1] = (dp1[i] * 2 + dp2[i]) % 10007; 
             }
         }
-        
-        return (dp0[n] + dp1[n]) % 10007;
+        return (dp1[n] + dp2[n]) % 10007;
     }
 }

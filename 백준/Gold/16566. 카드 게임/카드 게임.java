9a946a9ff8c79caad871baf137c334e1edd4de80
KIM TAEMIN <xoals8534@gmail.com>
2026-04-02 15:40:08 +0900

    [Gold I] Title: 카드 게임, Time: 1808 ms, Memory: 371484 KB -BaekjoonHub

--- 백준/Gold/16566. 카드 게임/카드 게임.java
+++ 백준/Gold/16566. 카드 게임/카드 게임.java
@@ -5,13 +5,8 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
 
-    static int[] parent;
-
-    static int find(int x) {
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-    
+    static int [] parent;
+    static int [] val;
     
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,47 +14,47 @@ class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int [] arr = new int[M];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0 ; i < M;i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        parent = new int[M + 1];
-        for (int i = 0;i <= M;i++) {
-            parent[i] = i;
-        }
 
-        Arrays.sort(arr);
         StringBuilder sb = new StringBuilder();
+
+        parent = new int[M + 1];
+        val = new int[M];
+        
+        
         st = new StringTokenizer(br.readLine());
-        for (int i = 0 ; i < K;i++) {
-            int a = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < M ;i++) {
+            parent[i] = i;
+            val[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(val);
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < K ;i++) {
+            int num = Integer.parseInt(st.nextToken());
+
             int lo = 0;
-            int hi = arr.length - 1;
-            int idx = 0;
+            int hi = M - 1;
+            int ans = 0;
+            
             while(lo <= hi) {
-                int mid = (lo+hi) / 2;
-                int l = arr[mid];
-                if (l > a) {
-                    idx = mid;
+                int mid = (lo + hi) / 2;
+                if (val[mid] > num) {
+                    ans = mid;
                     hi = mid - 1;
                 } else {
                     lo = mid + 1;
                 }
             }
-            idx = find(idx);
-            sb.append(arr[idx] + "\n");
-            
+            int idx = find(ans);
+            sb.append(val[idx] + "\n");
             parent[idx] = find(idx + 1);
-            
-            
-                
-            
             
         }
         System.out.print(sb);
-
     }
 
-    
+    public static int find(int x) {
+        if (x == parent[x]) return x;
+        return parent[x] = find(parent[x]);
+    }
 }

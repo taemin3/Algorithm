136e18eb96dae9217652467456e42293d04ce426
KIM TAEMIN <xoals8534@gmail.com>
2026-04-03 14:36:24 +0900

    [Gold IV] Title: 공유기 설치, Time: 284 ms, Memory: 29532 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -5,29 +5,26 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
 
-    static int[] arr;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
-        
-         arr = new int[N];
-        
-        for (int i = 0; i < N;i++) {
+
+        int [] arr = new int[N];
+
+        for (int i = 0; i < N ;i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-
         Arrays.sort(arr);
 
         int lo = 0;
-        int hi = arr[N - 1] - arr[0];
+        int hi = arr[N-1] - arr[0];
         int ans = 0;
+
         while(lo <= hi) {
             int mid = (lo + hi) / 2;
-            
-            if (check(mid,C,N)) {
+            if (can (arr,C,N,mid)) {
                 ans = mid;
                 lo = mid + 1;
             } else {
@@ -35,22 +32,25 @@ class Main {
             }
         }
         System.out.println(ans);
+    }
+
+    public static boolean can(int [] arr, int C, int N, int mid) {
+        int cnt = 1;
+        int idx = 0;
+        for (int i = 1 ; i < N;i++) {
+            if (arr[i] - arr[idx] >= mid) {
+                idx = i;
+                cnt++;
+            }
+            if (cnt >= C) {
+                return true;
+            }
+        }
+        return false;
         
     }
 
-    static boolean check(int k, int c,int N) {
-        int count = 1;
-        int idx = 0;
-        for (int i = 1 ; i < N ; i++) {
-            if (arr[i] - arr[idx] >= k) {
-                count++;
-                idx = i;
-            } 
-        }
-        if (count >= c) return true;
-        return false;
-    }
-
+    
+    
     
 }
-

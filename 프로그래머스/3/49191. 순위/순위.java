f79d1a5d7c52c8460418f0b26dee8d5d490ef262
KIM TAEMIN <xoals8534@gmail.com>
2026-05-08 23:48:08 +0900

    [level 3] Title: 순위, Time: 3.74 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,43 +1,35 @@
 class Solution {
     public int solution(int n, int[][] results) {
-        boolean[][] win = new boolean[n + 1][n + 1];
+        boolean [][] win = new boolean[n+1][n+1];
         
-        for (int [] r : results) { 
+        for (int [] r : results) {
             win[r[0]][r[1]] = true;
         }
         
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
+        for (int k = 1; k <= n;k++) {
+            for (int i = 1; i <= n;i++) {
                 if (!win[i][k]) {
                     continue;
                 }
-                for (int j = 1; j <= n;j++) {
+                
+                for (int j = 1;j <= n;j++){
                     if (win[k][j]) {
                         win[i][j] = true;
                     }
                 }
             }
-            
         }
-        
-        
-        
-        int answer = 0;
-        
-        for (int i =1;i <= n ;i++) {
-            int count = 0;
-            for (int j = 1;j <= n;j++) {
-                if (i == j) continue;
+        int ans = 0;
+        for (int i = 1;i <= n;i++){
+            int cnt = 0;
+            for (int j = 1;j<=n;j++) {
                 if (win[i][j] || win[j][i]) {
-                    count++;
+                    cnt++;
                 }
             }
-            
-            if (count == n - 1) {
-                answer++;
-            }
+            if (cnt == n - 1) ans++;
         }
         
-        return answer;
+        return ans;
     }
 }

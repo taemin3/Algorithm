50d530e7b39dd5b7a22a94222c759948fb892ad6
KIM TAEMIN <xoals8534@gmail.com>
2026-05-12 14:11:42 +0900

    [level 2] Title: 가장 큰 수, Time: 2922.49 ms, Memory: 392 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,24 +1,23 @@
 import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
         
         String [] numStr = new String[numbers.length];
         
-        for (int i = 0 ;i < numbers.length;i++) {
+        for (int i = 0; i < numbers.length;i++) {
             numStr[i] = Integer.toString(numbers[i]);
         }
         
-        Arrays.sort(numStr, (a,b) -> (b + a).compareTo(a+b));
+        Arrays.sort(numStr, (a,b) -> (b+a).compareTo(a+b));
         
-        if (numStr[0].equals("0")) {
+        if(numStr[0].equals("0")) {
             return "0";
         }
         
+        String answer = "";
         for (String s : numStr) {
             answer += s;
         }
-        
         return answer;
     }
 }

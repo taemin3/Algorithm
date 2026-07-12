ac3c1457bf562b0ea96026224d2ce81568577bcb
KIM TAEMIN <xoals8534@gmail.com>
2026-07-12 21:07:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.82 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,41 +1,48 @@
 import java.util.*;
 
 class Solution {
-    static boolean[][] visit;
-    static int n,m;
+    
+    static boolean [][] visit;
+    static int n, m;
+
     public int solution(int[][] maps) {
-        int answer = 0;
         n = maps.length;
         m = maps[0].length;
-        visit = new boolean[maps.length][maps[0].length];
-        
+        visit = new boolean[n + 1][m + 1];
         return bfs(maps);
     }
     
-    public int bfs(int[][] maps) {
-        Queue<int[]> queue = new LinkedList<>();
+    static int bfs(int[][] maps) {
+        Queue<int[]> q = new LinkedList<>();
         int [] dx = {1,-1,0,0};
         int [] dy = {0,0,1,-1};
-        queue.offer(new int[]{0,0,1});
-        visit[0][0] = true;
-        int count = 1;
-        while(!queue.isEmpty()) {
+        visit[1][1] = true;
+        q.offer(new int[]{1,1,1});
+
+        while(!q.isEmpty()) {
+            int [] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+            int cost = cur[2];
+            if (cx == n && cy == m) {
+                return cost;
+            };
             
-            int [] a = queue.poll();
-            if (a[0] == n - 1 && a[1] == m - 1) {
-                return a[2];
+            for (int i = 0;i < 4;i++){
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+                if (nx <= 0 || nx > n || ny <= 0 || ny >m) continue;
+                if (visit[nx][ny]) continue;
+                if (maps[nx - 1][ny - 1] == 0) continue;
+                visit[nx][ny] = true;
+                q.offer(new int[]{nx,ny,cost+1});
             }
-            for (int i = 0 ; i < 4 ; i++) {
-                int cx = a[0] + dx[i];
-                int cy = a[1] + dy[i];
-                if (cx >= 0 && cy >= 0 && cx < n && cy < m && maps[cx][cy] == 1 
-                    && !visit[cx][cy]) {
-                    visit[cx][cy] = true;
-                    queue.offer(new int[]{cx,cy,a[2] + 1});
-                    count++;
-                }
-            }
+            
         }
-        return -1;
+        return -1; 
+        
+        
     }
+   
+    
 }

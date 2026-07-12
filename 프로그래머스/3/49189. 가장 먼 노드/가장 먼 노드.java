4d699a6370d10a1049f3e5df38ecfa251cb4cdc5
KIM TAEMIN <xoals8534@gmail.com>
2026-07-12 21:57:32 +0900

    [level 3] Title: 가장 먼 노드, Time: 20.23 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -5,51 +5,46 @@ class Solution {
     boolean [] visit;
     
     public int solution(int n, int[][] edge) {
-    
-        graph = new ArrayList[n + 1];
-        for (int i = 1; i <= n;i++) {
+        graph = new ArrayList[n+1];
+        for (int i = 1; i <= n;i++){
             graph[i] = new ArrayList<>();
         }
-       
+        visit = new boolean[n+ 1];
         
         for (int [] e : edge) {
             graph[e[0]].add(e[1]);
             graph[e[1]].add(e[0]);
         }
-        return bfs(n);
+        return bfs();
         
+      
     }
     
-    public int bfs(int n) {
-        visit = new boolean[n + 1];
+    public int bfs() {
+        visit[1] = true;
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[]{1,0});
+        int cnt = 0;
         int max = 0;
-        int ans = 0;
-        visit[1] = true;
-        
         while(!q.isEmpty()) {
             int [] cur = q.poll();
             int now = cur[0];
             int cost = cur[1];
-            if (cost == max) {
-                ans++;
-            } else if (cost > max) {
+            if (cost > max) {
                 max = cost;
-                ans = 1;
+                cnt = 1;
+            } else if (cost == max) {
+                cnt++;
             }
-            
-            
-            
             for (int g : graph[now]) {
-                if (visit[g]) continue; 
+                if (visit[g]) continue;
                 visit[g] = true;
                 q.offer(new int[]{g, cost + 1});
             }
-            
         }
-        
-        return ans;
+        return cnt;
     }
+    
+   
     
 }
